3847d866428476552fdf8c92309e8d87b0631b55
ronakraj00 <12213052@nitkkr.ac.in>
2024-09-05 10:23:55 +0530

    add color

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NumberConverter from "./components/NumberConverter"
+import ExtractText from './components/ExtractText'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <NumberConverter/>
+      <ExtractText/>
     </>
   )
 }
--- src/components/NumberConverter.jsx
+++ src/components/NumberConverter.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import "../numberConverter.css"
+
 
 function NumberConverter() {
 
   const [input,setInput]=useState(0)
-  const buttons=["decimal","octadecimal","hexadecimal"]
+  const buttons=[("decimal","blue"),("octadecimal","green"),("hexadecimal","red")]
   const [answer,setAnswer]=useState()
 
   function handleInput(event){
@@ -32,9 +34,9 @@ function NumberConverter() {
           <input value={input} onChange={handleInput} placeholder="enter binary number"/>
         </div>
         <div>
-          {buttons.map(btn=>{
+          {buttons.map((btn,color)=>{
             return (
-              <button key={btn} onClick={()=>handleConvert(input,btn)}>
+              <button key={btn} style={{background:`${color}`}} onClick={()=>handleConvert(input,btn)}>
                 {btn}
               </button>
             )
